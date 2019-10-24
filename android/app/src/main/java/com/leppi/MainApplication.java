ef5b7e8c68b763004b030d412f1e9079b1726043
NewStar <newstar@gmail.com>
2019-10-24 15:34:44 +0300

    init project

--- android/app/src/main/java/com/leppi/MainApplication.java
+++ android/app/src/main/java/com/leppi/MainApplication.java
@@ -1,4 +1,4 @@
-package com.awesomeproject;
+package com.leppi;
 
 import android.app.Application;
 import android.content.Context;
--- ios/leppiTests/leppiTests.m
+++ ios/leppiTests/leppiTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AwesomeProjectTests : XCTestCase
+@interface leppiTests : XCTestCase
 
 @end
 
-@implementation AwesomeProjectTests
+@implementation leppiTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
